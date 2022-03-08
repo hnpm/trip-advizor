aa268cc250de9125a3161f63cfa7e0f292f7e6e7
Hien Pham <h13nph4m@gmail.com>
2022-03-08 18:21:14 +0700

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./components/Header";
+import List from "./components/List";
+import Map from "./components/Map";
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
