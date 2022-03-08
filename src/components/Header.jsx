d0ab250692a5e0036afc06847a64b36e7ea8eda3
Hien Pham <h13nph4m@gmail.com>
2022-03-08 18:48:25 +0700

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,34 @@
+import { AppBar, Box, InputBase, Toolbar, Typography } from '@material-ui/core';
+import { Search } from '@material-ui/icons';
+import { Autocomplete } from '@react-google-maps/api';
+import useStyles from './HeaderStyle';
+
 export default function Header() {
-  return <div>Header</div>;
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Trip Advisor
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <Search />
+            </div>
+            <InputBase
+              placeholder="Search..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
